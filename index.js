129a9166b60303009700dc51ed0dc88c71cd0199
Sajjad-Hosan <s4jj4dho34n@hmail.com>
2024-08-17 12:07:19 +0600

    git run function and middle ware

--- index.js
+++ index.js
@@ -8,7 +8,40 @@ const app = express();
 const port = 3000 || process.env.PORT;
 const uri = `mongodb://localhost:27017`;
 
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    const userCollection = client.db("ecopoShop").collection("users");
+    const productCollection = client.db("ecopoShop").collection("products");
+    const orderCollection = client.db("ecopoShop").collection("orders");
+    const paymentCollection = client.db("ecopoShop").collection("payments");
+    //
+    app.post("/add-user", async (req, res) => {
+      const user = req.body;
+      const exist = await userCollection.findOne({ email: user?.email });
+      if (exist) {
+        return res.send({ message: "user already exist!" });
+      }
+      const userAdd = await userCollection.insertOne(user);
+      res.send({ message: "success", userAdd });
+    });
+
+  } finally {
+    // console.log('')
+  }
+};
+run().catch(console.dir);
 
 app.get("/", async (req, res) => {
   res.send(`EvoPo Shop Server Running Now ${new Date().toLocaleTimeString()}`);
